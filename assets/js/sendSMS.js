43a7cdfdef1d8118f91e860e3f2ec1c59636d144
joshuabrink1 <u19185678@tuks.co.za>
2020-11-05 19:55:21 +0200

    tag logic

--- assets/js/sendSMS.js
+++ assets/js/sendSMS.js
@@ -38,7 +38,7 @@ const TagsInput = function (element) {
                 var tags = {};
                 for (var i = 0; i < elements.length; i++) {
                     element = elements[i]
-                    tags[element.innerText] = element;
+                    tags[element.innerText.split('\n')[0]] = element;
                 }
 
                 return tags;
@@ -70,12 +70,30 @@ const TagsInput = function (element) {
         tag = tag.replace(initCharPattern, '');
         tag = tag.replace(/^\s+/, '').replace(/\s+$/, ''); //replace one or more white spaces and newlines
         // tag = tag.replace(/^0/, '27'); //replace 0 with 27 if number begins with 0
-        tag = tag[0].toUpperCase() + tag.toLowerCase().slice(1);
+        // tag = tag[0].toUpperCase() + tag.toLowerCase().slice(1);
         if (tag != '' && this.tags[tag] === undefined) {
             var element = document.createElement('span');
-            element.appendChild(document.createTextNode(tag));
+            // element.appendChild(document.createTextNode(tag));
             element.setAttribute('contenteditable', 'false');
-            element.classList.add('number')
+            // element.classList.add('number')
+
+           
+            var top = document.createElement('div')
+            top.classList.add('top')
+            top.appendChild(document.createTextNode(tag))
+            var under = document.createElement('div')
+            under.classList.add('under');
+
+            let number = document.querySelector(`input[name="${tag}"]`).dataset.number;
+            if(number.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g)) {
+                element.classList.add('cross-fade','c-tag')
+            } else {
+                element.classList.add('cross-fade','g-tag')
+            }
+
+            under.appendChild(document.createTextNode(number))
+            element.appendChild(top);
+            element.appendChild(under);
 
             insert(element);
         }
@@ -159,11 +177,18 @@ const showSMSResponse = (parent, data) => {
         void parent.offsetWidth;
 
         parent.innerHTML = '<h5>Text message sent to: ';
-        data.numbers.forEach(number => {
-            parent.innerHTML += number + ' '
-        });
+        for (let i = 0; i < data.contacts.length; i++) {
+            parent.innerHTML += data.contacts[i].name + ' '
+            
+        }
         parent.innerHTML += '</h5>';
 
+        parent.innerHTML += '<h5>';
+        for (let i = 0; i < data.groups.length; i++) {
+            parent.innerHTML += data.groups[i] + ' '
+            
+        }
+        parent.innerHTML += '</h5>';
         parent.classList.add('fadeIn')
     }, 1000);
 }
@@ -181,11 +206,19 @@ try {
 // MAIN SMS SEND FUNCTION
 
 function sendSMS() {
-    const numbersInput = document.querySelectorAll('.number');
-    const numbers = Array.from(numbersInput).map(number => {
-        number.innerText = number.innerText.replace(/^0/g, '27')
-        return number.innerText.replace(/\D/g, '')
+
+    const contactsInput = numberInput.querySelectorAll('.c-tag');
+    const groupsInput = numberInput.querySelectorAll('.g-tag');
+
+    const contacts = Array.from(contactsInput).map(contact => {
+        // number.innerText = number.innerText.replace(/^0/g, '27')
+        return {name:contact.innerText.split('\n')[0],
+            number:contact.innerText.split('\n')[1].replace(/\D/g, '')}
     })
+
+    const groups = Array.from(groupsInput).map(group => {
+        return group.innerText.split('\n')[0]
+    });
 
     const textInput = document.getElementById('msg')
     const message = textInput.value;
@@ -197,6 +230,99 @@ function sendSMS() {
 
     showPreloader(response)
 
-    asyncReq('/sendSMS', 'post', { numbers, message }, finish)
+
+    asyncReq('/addMessage', 'post', { contacts, groups, message, async:true }, finish)
 
 }
+
+function toggleTagList(container) {
+
+    let searchBar = container.querySelector('.search');
+    let toggleBtn = container.querySelector('.toggle-btn');
+    let textToggle = container.querySelector('.toggle-title');
+    let headerCol = container.querySelector('thead tr').lastElementChild;
+    // let body = container.getElementById('contact-list');
+
+    toggleBtn.addEventListener('click', e => {
+        e.preventDefault();
+        let changeTo = toggleBtn.innerText.toLowerCase();
+        asyncReq(`/${changeTo}Filter`, 'post', { async: true }, (data)=> {
+
+            data = data.data;
+
+            let rows = ``;
+
+            for (let i = 0; i < data.length; i++) {
+             
+                if(changeTo == 'groups') {  
+                    rows += `<tr>
+                    <td style="padding-left: 45px;">
+                        <div class="form-check">
+                            <input class="form-check-input position-static" 
+                            type="checkbox" name="${data[i].name}" data-number="${data[i].contacts.length}">
+                        </div>
+                    </td>`        
+                    rows +=  `<td>
+                        ${data[i].name}
+                    </td>
+                    <td>
+                        ${data[i].contacts.length}
+                    </td>
+                    </tr>` 
+                } else {
+                    rows += `<tr>
+                    <td style="padding-left: 45px;">
+                        <div class="form-check">
+                            <input class="form-check-input position-static" 
+                            type="checkbox" name="${data[i].name}" data-number="${data[i].number}">
+                        </div>
+                    </td>` 
+                    rows +=  `<td>
+                    ${data[i].name}
+                    </td>
+                    <td>
+                    ${data[i].number}
+                    </td>
+                    </tr>` 
+                }
+            }
+
+            let upperCase = changeTo[0].toUpperCase() + changeTo.slice(1);
+
+            toggleBtn.innerText = textToggle.innerText;
+            if(changeTo == 'groups') {
+                headerCol.innerHTML = `Count`
+                textToggle.innerHTML = `<i class="fa fa-group"></i> 
+                ${upperCase}`;
+                f.type = 'groups'
+            } else {
+                headerCol.innerHTML = `Number <a id="number-sort" class="col-sort" href=""><i class="fa fa-sort"></i></a>`
+                textToggle.innerHTML = `<i class="fa fa-address-book"></i> 
+                ${upperCase}`;
+                f.type = 'contacts'
+            }
+
+            // textToggle.innerHTML = `<i class="fa 
+            // ${changeTo == 'contacts' ? 'fa-address-book': 'fa-group'}"></i> 
+            // ${upperCase}`;
+
+            searchBar.setAttribute('action', `/search${upperCase}`)
+
+            filterBody.innerHTML = rows;
+
+            let checkList = filterBody.children;
+            for (let i = 0; i < checkList.length; i++) {
+              tagListen(checkList[i].querySelector(".form-check-input"));
+      
+            }
+
+        })
+
+    })
+
+
+
+}
+
+toggleTagList(parent)
+
